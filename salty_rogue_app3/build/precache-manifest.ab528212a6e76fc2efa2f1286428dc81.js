96ba5c1948be7feb3a4907e4c06be7e8fc820864
TylerOlsen14 <Tyler.Olsen1@gmail.com>
2019-04-25 12:29:14 -0600

    ran npm build

---
+++ salty_rogue_app3/build/precache-manifest.ab528212a6e76fc2efa2f1286428dc81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "52b14cf99ed0b81a02363535862e749f",
+    "url": "/static/media/logo.52b14cf9.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6590d374704833246ead",
+    "url": "/static/js/main.86f04db8.chunk.js"
+  },
+  {
+    "revision": "de66b6df029d061ea54e",
+    "url": "/static/js/2.e1753ced.chunk.js"
+  },
+  {
+    "revision": "6590d374704833246ead",
+    "url": "/static/css/main.02e2fed5.chunk.css"
+  },
+  {
+    "revision": "b503e89a32403543c3ea3fb7da503d0f",
+    "url": "/index.html"
+  }
+];
